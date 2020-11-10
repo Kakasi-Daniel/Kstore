38ceefc35f5cccfa7120bf01c84ec554e207ea76
Kakasi Daniel <danielkksi@Kakasis-iMac.local>
2020-11-10 14:54:04 +0200

    first commit

---
+++ precache-manifest.820052f2c7d471c60e05128de8c7aa71.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66b3d98b4a07957008c162bada1c0f71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c745f1c9f5669a1ae3fe",
+    "url": "./static/css/main.16d08d31.chunk.css"
+  },
+  {
+    "revision": "778fecbf25f85ea0fddc",
+    "url": "./static/js/2.4a48520d.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "./static/js/2.4a48520d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c745f1c9f5669a1ae3fe",
+    "url": "./static/js/main.3a9fbbf0.chunk.js"
+  },
+  {
+    "revision": "5a86810bd5e174441f22",
+    "url": "./static/js/runtime-main.26914bb4.js"
+  },
+  {
+    "revision": "a3b40bad8a600c96e0b6ba3addd95fdd",
+    "url": "./static/media/Untitled-1.a3b40bad.jpg"
+  },
+  {
+    "revision": "f9d058540dac75cc772910b4c474f7ab",
+    "url": "./static/media/Untitled-2.f9d05854.jpg"
+  },
+  {
+    "revision": "c7c40fc7c04bddf53c2d74aa9ca4b9a6",
+    "url": "./static/media/Untitled-3.c7c40fc7.jpg"
+  },
+  {
+    "revision": "77c9c8ffa45e040c06ad0d4be8b045b1",
+    "url": "./static/media/Untitled-4.77c9c8ff.jpg"
+  },
+  {
+    "revision": "2d9dc5c35bad2b89084e28d906b4a2c9",
+    "url": "./static/media/kstorelogo.2d9dc5c3.png"
+  }
+]);
